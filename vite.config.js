d15bbf5b39a5f1fff463324e5f1ce9020d941427
Erik Trefzger <mbairm3@eriks-air.lan>
2025-01-26 15:11:16 -0800

    Fix MIME type issues and asset handling

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,12 @@ export default defineConfig({
   base: '/Remixer/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
-      input: {
-        main: './index.html'
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
       }
     }
   },
