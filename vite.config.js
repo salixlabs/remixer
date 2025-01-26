c8284403dea1a0263a2041f635d8798b05cc8b54
Erik Trefzger <mbairm3@eriks-air.lan>
2025-01-26 14:41:31 -0800

    github pages fixes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Remixer/', // Replace with your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
